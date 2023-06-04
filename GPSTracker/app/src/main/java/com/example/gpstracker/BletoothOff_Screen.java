02e00caa43259e87c99ce3776a89ab813ee810cf
DESKTOP-C70KB89\ASUS <20520735@gm.uit.edu.vn>
2023-06-04 16:53:37 +0700

    no button to turn on bluetooth

--- GPSTracker/app/src/main/java/com/example/gpstracker/BletoothOff_Screen.java
+++ GPSTracker/app/src/main/java/com/example/gpstracker/BletoothOff_Screen.java
@@ -15,56 +15,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class BletoothOff_Screen extends AppCompatActivity {
 
-      private boolean check_BLEon= false;
-      final private ActivityResultLauncher<Intent> enableBtLauncher = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
-                @Override
-                public void onActivityResult(ActivityResult result) {
-                    if (result.getResultCode()== Activity.RESULT_OK){
-                        //
-                        //Bluetooth is enabled, handle the success case
-                        check_BLEon= true;
 
-                    } else {
-                        //Bluetooth is not enabled, handle the failure case
-                        //
-                    }
-                }
-            }
-    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainscreen);
 
-        final Button btnOn = findViewById(R.id.bthBluetoothOn);
+
 
         final BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (!mBluetoothAdapter.isEnabled()) {
+             Intent FindDevice_screen = new Intent(this,FindDevice.class);
+             startActivity(FindDevice_screen);
 
-
-        btnOn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                if (!mBluetoothAdapter.isEnabled()) {
-                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                    enableBtLauncher.launch(enableBtIntent);
-                   // startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-                       // Intent FindDevice_screen = new Intent(v.getContext(),FindDevice.class);
-                    //    startActivity(FindDevice_screen);
-
-                }
-
-
-            }
-           // Intent FindDevice_screen = new Intent(this.,FindDevice.class);
-           // startActivity(FindDevice_screen);
-        });
-
-       // Intent FindDevice_screen = new Intent(this,FindDevice.class);
-       // startActivity(FindDevice_screen);
         }
 
 
 
+
+
     }
+}
+
